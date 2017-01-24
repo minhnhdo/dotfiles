af5216353bd093f1e85228357a15f46807fa53a0
Do Nhat Minh <mrordinaire@gmail.com>
2017-01-24 22:26:54 +0800

    added taskrc

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # verbose and exit on error
 set -xe
 
-for f in gitconfig jshintrc spacemacs tmux.conf vimrc zshrc
+for f in gitconfig jshintrc spacemacs tmux.conf vimrc zshrc taskrc
 do
   ln -s $PWD/$f $HOME/.$f
 done
